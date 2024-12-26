a1a8531e064583bf029db7d97845886ad84b7ac3
Mohammed Hawrami <hawramimohammed@gmail.com>
2024-12-26 14:09:15 +0100

    Update page.js

--- src/app/impressum/page.js
+++ src/app/impressum/page.js
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 
-import MainGrid from '@/components/MainGrid';
+import MainGrid from '@/components/MainGridImp';
 
 export default async function Home() {
   const file = await fs.readFile(process.cwd() + '/src/data.json', 'utf8');
