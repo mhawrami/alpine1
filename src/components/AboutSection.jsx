950fddd9c296dd10b614efd4555f279ce5ef2893
Mohammed Hawrami <hawramimohammed@gmail.com>
2024-12-26 16:04:20 +0100

    Create AboutSection.jsx

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import styles from './AboutSection.module.css'; // Import CSS module
+
+const AboutSection = () => {
+  const [showImprint, setShowImprint] = useState(false);
+
+  const handleImpressumClick = () => {
+    setShowImprint(!showImprint);
+  };
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#" onClick={handleImpressumClick}>
+              {showImprint ? 'Über mich' : 'IMPRESSUM'}
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.textContainer}>
+        <p className={showImprint ? styles.fadeOut : styles.fadeIn}>
+          {showImprint
+            ? 'Your company imprint information goes here.'
+            : 'Ich bin Moe und dein Partner für alles rund um Webentwicklung, Design und IT-Consulting. Mein Ziel ist es, dich mit modernen und nachhaltigen IT-Lösungen zu unterstützen, damit dein Business auf die nächste Stufe gehoben wird.'}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
