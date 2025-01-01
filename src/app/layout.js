eeef0f28cfa5effcf45b9e138c864922aca57db9
Mohammed Hawrami <hawramimohammed@gmail.com>
2025-01-01 02:20:09 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { DM_Sans, DM_Serif_Display } from 'next/font/google';
 import Global from '@/components/Global';
-import { LanguageProvider } from '@/context/LanguageContext'; // Importiere LanguageProvider
+import { LanguageProvider, useLanguage } from '@/context/LanguageContext'; // Importiere LanguageProvider und Hook
+import dataDe from '@/data.de.json';
+import dataEn from '@/data.en.json'; // Importiere die lokalisierten Daten
 import './globals.css';
 
 export const metadata = {
@@ -25,15 +27,29 @@ const fontBody = DM_Sans({
   variable: '--font-body',
 });
 
+// Komponente zur Bereitstellung von lokalisierten Daten
+function LocalizationProvider({ children }) {
+  const { language } = useLanguage(); // Aktuelle Sprache aus dem Kontext
+  const localizedData = language === 'de' ? dataDe : dataEn;
+
+  return (
+    <LocalizedDataContext.Provider value={localizedData}>
+      {children}
+    </LocalizedDataContext.Provider>
+  );
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className='hide-scrollbar'>
       <body
         className={`${fontHeading.variable} ${fontBody.variable} antialiased`}
       >
-        <LanguageProvider> {/* Umhülle den gesamten Inhalt */}
-          <Global />
-          {children}
+        <LanguageProvider> {/* Umhülle den gesamten Inhalt mit LanguageProvider */}
+          <LocalizationProvider> {/* Umhülle mit LocalizationProvider */}
+            <Global />
+            {children}
+          </LocalizationProvider>
         </LanguageProvider>
       </body>
     </html>
