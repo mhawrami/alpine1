e67c2df4fbd50efd1f3772450457fa7bbbf7cb17
Mohammed Hawrami <hawramimohammed@gmail.com>
2025-01-15 19:19:49 +0100

    a

--- src/context/LocalizedDataContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const LocalizedDataContext = createContext(null);
-
-export const useLocalizedData = () => useContext(LocalizedDataContext);
